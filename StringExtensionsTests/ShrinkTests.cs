6bb2847317391bb0e7ad987708f65cc7a19d2980
Marek Grochowski <marekgrochowski@web.de>
2020-06-04 11:13:42 +0200

    Tests added

---
+++ StringExtensionsTests/ShrinkTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using StringExtensions;
+
+namespace StringExtensionsTests
+{
+    public class ShrinkTests
+    {
+        #region Public Methods
+
+        [Test]
+        public void ShortenWithWhitespace()
+        {
+            var result = ("Ulm Hbf").Shorten(10);
+
+            Assert.IsTrue(result.Contains(" ", comparisonType: System.StringComparison.InvariantCulture));
+        }
+
+        [Test]
+        public void ShrinkWithWhitespace()
+        {
+            var result = ("Ulm Hbf").Shrink(10);
+
+            Assert.IsFalse(result.Contains(" ", comparisonType: System.StringComparison.InvariantCulture));
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ StringExtensionsTests/SplitTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using StringExtensions;
+using System.Linq;
+
+namespace StringExtensionsTests
+{
+    public class SplitTests
+    {
+        #region Public Methods
+
+        [Test]
+        public void SplitEmptyWithCharSeparatorToInt()
+        {
+            var result = (default(string)).Split<int>(',').ToArray();
+
+            Assert.IsTrue(result.Count() == 0);
+        }
+
+        [Test]
+        public void SplitEmptyWithCharSeparatorToString()
+        {
+            var result = (default(string)).Split<string>(',').ToArray();
+
+            Assert.IsTrue(result.Count() == 0);
+        }
+
+        [Test]
+        public void SplitEmptyWithStringSeparatorToInt()
+        {
+            var result = (default(string)).Split<int>(",;").ToArray();
+
+            Assert.IsTrue(result.Count() == 0);
+        }
+
+        [Test]
+        public void SplitEmptyWithStringSeparatorToString()
+        {
+            var result = (default(string)).Split<string>(",;").ToArray();
+
+            Assert.IsTrue(result.Count() == 0);
+        }
+
+        [Test]
+        public void SplitWithCharSeparatorToInt()
+        {
+            var result = ("1,2,3").Split<int>(',').ToArray();
+
+            Assert.IsTrue(result.Count() == 3);
+        }
+
+        [Test]
+        public void SplitWithCharSeparatorToString()
+        {
+            var result = ("1,2,3").Split<string>(',').ToArray();
+
+            Assert.IsTrue(result.Count() == 3);
+        }
+
+        [Test]
+        public void SplitWithStringSeparatorToInt()
+        {
+            var result = ("1,2;3").Split<int>(",;").ToArray();
+
+            Assert.IsTrue(result.Count() == 3);
+        }
+
+        [Test]
+        public void SplitWithStringSeparatorToString()
+        {
+            var result = ("1,2;3").Split<string>(",;").ToArray();
+
+            Assert.IsTrue(result.Count() == 3);
+        }
+
+        #endregion Public Methods
+    }
+}
